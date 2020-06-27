1002b7933d20f476f3b0ac5a0fdf2dcfce6eeee8
Lambda_School_Loaner_271 <lambda_school_loaner_271@Lambda-School-Loaner-271s-MacBook-Pro.local>
2020-06-26 21:47:55 -0400

    running out of time on project

--- index.js
+++ index.js
@@ -207,12 +207,13 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
-
+console.log(artists[0].name);
+console.log(artists[3].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+ artists[9].name = "Vincent Van Gogh";
+ console.log(artists[9].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,12 +223,16 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(array, index) {
-    /* code here */
+function getArtistByIndex(array, num) {
+  for(let i =0; i< array.length; i++){
+    array[i].num
+   return " The artist at index " + num + " is " + array[num].name;
   }
-  
-  /**
-
+ 
+}
+getArtistByIndex(artists,0);
+console.log(getArtistByIndex(artists,0));
+ 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
@@ -248,10 +253,11 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array, num) {
+  array.splice[num]
+  console.log(array[num].length);
   }
-  
+  removeArtist(artists, 0)
  
 
 /**
